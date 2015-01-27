2fca63a0bfe655db021f248e6c55bb3345c25b01
Juhani <juhani.niinimaa@myy.haaga-helia.fi>
2015-01-27 11:53:54 +0200

    init

---
+++ HelloWorld.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - Git test
+
+import UIKit
+
+
+//TUPLES
+func tuples(pisteet: [Int]) -> (max: Int, min: Int){
+    
+    var max = pisteet[0]
+    var tokamax = pisteet[0]
+    
+    for var i = 0; i < pisteet.count; i++
+    {
+        if pisteet[i] > max
+        {
+            tokamax = max
+            max = pisteet[i]
+        } else if pisteet[i] > tokamax{
+            tokamax = pisteet[i]
+        }
+    }
+    
+    
+    return (max, tokamax)
+}
+
+let vakioArray = tuples([5,10,15,20,55,25,30,35,600,43])
+print(vakioArray)
